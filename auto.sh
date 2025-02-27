fc42d7101fe5567626f205aa2e13956c62e4f80a
Tom Monaghan <tom.monaghan6@gmail.com>
2025-02-27 13:54:02 +0000

    V0.2, Added invisible AOI

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH="/mnt/system/lib:/mnt/system/usr/lib:/mnt/system/usr/lib/3rd"
+
+# pedestrian detector { program - video feed - model - conf threshold (optional, default 0.5)
+#/root/pedestrianDetector mobiledetv2-pedestrian /root/mobiledetv2-pedestrian-d0-ls-448.cvimodel 0.6
+
+# Put the programs you want to run automatically in here
